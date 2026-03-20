756040e3c663eba0723a270f241bd384e1cc2d60
DominicDev <s0l0estudi4r@gmail.com>
2026-03-20 17:35:38 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ROXETTE';
+export let nameSongs = [
+	"IT MUST HAVE BEEN LOVE", /*1*/
+	"LISTEN TO YOUR HEART", /*2*/
+	"SPENDING MY TIME", /*3*/
+	"JOYRIDE", /*4*/
+	"THE LOOK", /*5*/
+	"DRESSED FOR SUCCESS", /*6*/
+	"DANGEROUS", /*7*/
+	"VULNERABLE", /*8*/
+	"CRASH! BOOM! BANG!", /*9*/
+	"FADING LIKE A FLOWER", /*10*/
+	"THING WILL NEVER BE THE SAME", /*11*/
+	"DO YOU GET EXCITED?", /*12*/
+	"THE BIG L.", /*13*/
+	"CHANCES", /*14*/
+	"SLEEPING SINGLE", /*15*/
+	"SHADOW OF A DOUBT", /*16*/
+	"WISH I COULD FLY", /*17*/
+	"ANYONE", /*18*/
+	"SALVATION", /*19*/
+	"COOPER", /*20*/
+]
